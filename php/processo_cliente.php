03ecae397573f3e63657393b4fc5cc46f86540eb
Vicenzo <vicenzofr@gmail.com>
2023-07-29 08:49:34 -0300

    pronto
    sadw

---
+++ php/processo_cliente.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once("../php/db.php");
+require_once("../php/globals.php");
+require_once("../php/ClienteModel.php");
+require_once("../php/msg.php");
+require_once("../php/ClienteDAO.php");
+
+// Verificando o tipo do formulário
+$type = filter_input(INPUT_POST, "type");
+
+$message = new Message($BASE_URL);
+$clienteDao = new ClienteDao($conn, $BASE_URL);
+
+
+
+if ($type === "register") {
+
+  $nome = filter_input(INPUT_POST, "nome_clie");
+  $login = filter_input(INPUT_POST, "login");
+  $senha = filter_input(INPUT_POST, "senha");
+
+
+
+
+
+  // Verificação de dados mínimos 
+  if ($nome && $login && $senha) {
+
+    // Verificar se as senhas batem
+    // if ($senha === $senha) {
+
+    // Verificar se o e-mail já está cadastrado no sistema
+
+
+    $cliente = new Cliente();
+
+    $cliente->nome_clie = $nome;
+    $cliente->login = $login;
+    $cliente->senha = $senha;
+
+    $auth = true;
+
+    $clienteDao->criar_clie($cliente, $auth);
+    $message->setMessage("Usuário cadastrado, seja bem vindo!.", "sucess", "catalogo.html");
+
+    // } else {
+
+    //   // Enviar uma msg de erro, de senhas não batem
+    //   $message->setMessage("A senha não foi digitada.", "error", "back");
+    // }
+  } else {
+
+    // Enviar uma msg de erro, de dados faltantes
+    $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
+  }
+} elseif ($type === "login") {
+
+  $login = filter_input(INPUT_POST, "login");
+  $senha = filter_input(INPUT_POST, "senha");
+
+
+  $retorno = $clienteDao->authenticateUser($login, $senha);
+
+  // Tenta autenticar usuário
+  if ($retorno['erro'] !==  true) {
+
+    $_SESSION['usuario'] = $retorno['usuario'];
+    $message->setMessage("Seja bem-vindo!", "success", "../html/catalogo.html");
+
+    // Redireciona o usuário, caso não conseguir autenticar
+  } else {
+
+    $message->setMessage("Usuário e/ou senha incorretos.", "error", "");
+  }
+}
